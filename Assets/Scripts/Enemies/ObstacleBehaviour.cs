9b3c3063c0c835822006243a9c37dd41dc7c6985
Andrey <1929999@gmail.com>
2021-11-18 23:04:17 +0300

    ObstaclesBetter

--- Assets/Scripts/Enemies/ObstacleBehaviour.cs
+++ Assets/Scripts/Enemies/ObstacleBehaviour.cs
@@ -6,12 +6,30 @@ public class ObstacleBehaviour : MonoBehaviour
 {
     [SerializeField] private Canvas infoCanvas;
     [SerializeField] private GameObject removingWalls;
+    [SerializeField] private EventHandler openButton;
+    [SerializeField] private GameObject effectExplosion;
+    private bool isInSensor = false;
+    private bool isSelfDamaged = false;
+
+    private void Start()
+    {
+        if (infoCanvas == null || openButton == null) isSelfDamaged = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            infoCanvas.enabled = true;
+            if (!isSelfDamaged)
+            {
+                infoCanvas.enabled = true;
+                isInSensor = true;
+            }
+            else
+            {
+                effectExplosion.SetActive(true);                
+                removingWalls.SetActive(false);
+            }
         }
     }
 
@@ -19,7 +37,19 @@ public class ObstacleBehaviour : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            isInSensor = false;
             infoCanvas.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isInSensor && (Input.GetKey(KeyCode.E) || openButton.isDown))
+        {
+            effectExplosion.SetActive(true);
+            infoCanvas.enabled = false;
+            removingWalls.SetActive(false);
+
         }
     }
 }
