e65f7f64c98ecc0f7a141717dd7ee24d0f174c00
AndreyPikes <1929999@gmail.com>
2021-12-28 19:33:38 +0300

    Смешанный свет + отключен блум
    На моем работает нормально

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    public Text text;
+    private int x;
+    void Update()
+    {
+        x = (int)(1f / Time.unscaledDeltaTime);
+        if (x < 50) text.text = x.ToString();
+    }
+}
