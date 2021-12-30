d0a6af862e6283f8afd1b13e478e1bd329f640a3
AndreyPikes <1929999@gmail.com>
2021-12-30 12:45:39 +0300

    Оформление игры

---
+++ Assets/Scripts/UI/GameVersionToText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameVersionToText : MonoBehaviour
+{
+    [SerializeField] Text text;
+
+    void Start()
+    {
+        text.text = "ver. " + Application.version;
+    }
+
+    
+}
